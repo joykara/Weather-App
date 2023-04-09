575f9e21f573587a8da3d0279edfc5f8117adfe7
joykara <joykarani1@gmail.com>
2023-04-09 20:38:50 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const apiKey = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=53d1da66f8f7838016bdd48fd7c1ca6e";
+//uses the OpenWeatherMap API
+
+const searchBtn = document.querySelector("button");
+const searchInput = document.querySelector("input");
+const locationEl = document.querySelector(".location");
+const temperatureEl = document.querySelector(".temperature");
+const conditionsEl = document.querySelector(".conditions");
+const humidityEl = document.querySelector(".humidity");
+const windSpeedEl = document.querySelector(".wind-speed");
+
+searchBtn.addEventListener("click", getWeather);
+
+function getWeather() {
+  const location = searchInput.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      locationEl.textContent = `Location: ${data.name}, ${data.sys.country}`;
+      temperatureEl.textContent = `Temperature: ${(data.main.temp - 273.15).toFixed(1)}°C`;
+      conditionsEl.textContent = `Conditions: ${data.weather[0].description}`;
+      humidityEl.textContent = `Humidity: ${data.main.humidity}%`;
+      windSpeedEl.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+    })
+    .catch(error => console.log(error));
+}
